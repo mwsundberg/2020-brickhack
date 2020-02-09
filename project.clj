e0bfd92da308f7f15a9059546c23afcd54033754
Matthew Sundberg <matthew@mws.is>
2020-02-08 19:52:17 -0500

    basic setup

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject brickhack "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [quil "3.1.0"]])
